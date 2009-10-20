07d7220e0eb2bda854787a7fdbcbcdf3658fb662
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2009-10-20 03:13:55 +0000

    Replace all COPYRIGHT POD sections with a COPYRIGHT AND LICENSE section like the one recommended at http://www.perlfoundation.org/cpan_licensing_guidelines
    git-svn-id: file:///Users/tom/Code/svn/repos/PPIx-Regexp/trunk@36 f6fd3633-1913-47c2-bad4-372d75c948ec

--- lib/PPIx/Regexp/Constant.pm
+++ lib/PPIx/Regexp/Constant.pm
@@ -134,9 +134,17 @@ L<http://rt.cpan.org>, or in electronic mail to the author.
 
 Thomas R. Wyant, III F<wyant at cpan dot org>
 
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
-Copyright 2009 by Thomas R. Wyant, III.
+Copyright (C) 2009, Thomas R. Wyant, III
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl 5.10.0. For more details, see the full text
+of the licenses in the directory LICENSES.
+
+This program is distributed in the hope that it will be useful, but
+without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.
 
 =cut
 
