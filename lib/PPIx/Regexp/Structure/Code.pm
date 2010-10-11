de38fe8d0ff9b2392edfc35b048a37e2b174dc11
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2010-10-11 00:00:03 +0000

    Add a __PPIX_LEXER__finalize() method to PPIx::Regexp::Structure::Code. This scans the children() of the structure, accepting the first PPIx::Regexp::Token::Code, but rejecting anything else by counting an error and either reblessing it into the appropriate ::Unknown class if there is one, or cluck()-ing voluminously otherwise.
    The point of this is that in the (?{...}) construction, Perl requires
    the '}' and ')' to be adjacent, but PPIx::Regexp was accepting
    characters between them.
    
    
    git-svn-id: file:///Users/tom/Code/svn/repos/PPIx-Regexp/trunk@2047 f6fd3633-1913-47c2-bad4-372d75c948ec

--- lib/PPIx/Regexp/Structure/Code.pm
+++ lib/PPIx/Regexp/Structure/Code.pm
@@ -39,7 +39,41 @@ use warnings;
 
 use base qw{ PPIx::Regexp::Structure };
 
+use Carp qw{ cluck };
+use PPIx::Regexp::Constant qw{ STRUCTURE_UNKNOWN TOKEN_UNKNOWN };
+
 our $VERSION = '0.012';
+
+# The only child of this structure should be a single
+# PPIx::Regexp::Token::Code. Anything else gets turned into the
+# appropriate ::Unknown object.
+sub __PPIX_LEXER__finalize {
+    my ( $self ) = @_;
+
+    my $count;
+    my $errors = 0;
+
+    foreach my $kid ( $self->children() ) {
+
+	$kid->isa( 'PPIx::Regexp::Token::Code' )
+	    and not $count++
+	    and next;
+
+	$errors++;
+
+	if ( $kid->isa( 'PPIx::Regexp::Token' ) ) {
+	    bless $kid, TOKEN_UNKNOWN;
+	} elsif ( $kid->isa( 'PPIx::Regexp::Structure' ) ) {
+	    bless $kid, STRUCTURE_UNKNOWN;
+	} else {
+	    cluck( 'Programming error - unexpected element of class ',
+		ref $kid, ' found in a PPIx::Regexp::Structure::Code. ',
+		'Please contact the author' );
+	}
+
+    }
+    return $errors;
+}
 
 1;
 
