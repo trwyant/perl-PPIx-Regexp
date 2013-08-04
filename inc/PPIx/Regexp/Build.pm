bc85dda5e069ed117230b4852ea8903ae2ed7347
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2013-08-04 04:01:39 +0000

    Have the authortest build command set environment variable AUTHOR_TESTING to 1.
    git-svn-id: file:///Users/tom/Code/svn/repos/PPIx-Regexp/trunk@4025 f6fd3633-1913-47c2-bad4-372d75c948ec

--- inc/PPIx/Regexp/Build.pm
+++ inc/PPIx/Regexp/Build.pm
@@ -13,6 +13,8 @@ use Carp;
 sub ACTION_authortest {
     my ( $self, @args ) = @_;
 
+    local $ENV{AUTHOR_TESTING} = 1;
+
     $self->depends_on( 'build' );
     $self->test_files( qw{ t xt/author } );
     $self->depends_on( 'test' );
