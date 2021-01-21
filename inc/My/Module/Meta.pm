03f1d16ba5f9e133d1c556e465aed7cc7ced8ee0
Tom Wyant <wyant@cpan.org>
2021-01-21 16:52:26 -0500

    More updating of dependencies.

--- inc/My/Module/Meta.pm
+++ inc/My/Module/Meta.pm
@@ -72,9 +72,11 @@ sub requires {
 ##  }
     return {
         'Carp'			=> 0,
+	'Encode'		=> 0,
         'Exporter'		=> 0,
         'List::Util'		=> 0,
 	'PPI::Document'		=> 1.117,	# for new( readonly => 1 )
+	'PPI::Dumper'		=> 1.117,
         'Scalar::Util'		=> 0,
 	'Task::Weaken'		=> 0,
 	'Text::Tabs'		=> 0,
