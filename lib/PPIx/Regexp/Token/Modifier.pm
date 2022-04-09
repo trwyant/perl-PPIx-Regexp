94e34a7bc9858c5dd9369311da4ec6741c94b519
Tom Wyant <wyant@cpan.org>
2022-04-09 09:21:39 -0400

    Version 0.084_01
    Set version to 0.084_01. Update Changes file for changes since v00.084.

--- lib/PPIx/Regexp/Token/Modifier.pm
+++ lib/PPIx/Regexp/Token/Modifier.pm
@@ -105,7 +105,7 @@ use PPIx::Regexp::Constant qw{
     @CARP_NOT
 };
 
-our $VERSION = '0.084';
+our $VERSION = '0.084_01';
 
 # Define modifiers that are to be aggregated internally for ease of
 # computation.
